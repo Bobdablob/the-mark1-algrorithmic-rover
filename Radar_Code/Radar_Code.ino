11619324d2bff4638f3c4aec08df575e70b92384
Reagan G <mx3designs@cox.net>
2019-01-21 14:37:57 -0700

    Added the setup for the motors.

--- Radar_Code/Radar_Code.ino
+++ Radar_Code/Radar_Code.ino
@@ -8,7 +8,31 @@ long duration;
 int distance;
 Servo myServo; // Creates a servo object for controlling the servo motor
 
+// Clockwise and counter-clockwise definitions.
+// Depending on how you wired your motors, you may need to swap.
+#define FORWARD 0
+#define REVERSE 1
+
+// Motor definitions to make life easier:
+#define MOTOR_A 0
+#define MOTOR_B 1
+
+// Pin Assignments //
+//Default pins:
+#define DIRA 8 // Direction control for motor A
+#define PWMA 9  // PWM control (speed) for motor A
+#define DIRB 7 // Direction control for motor B
+#define PWMB 10 // PWM control (speed) for motor B
+
+////Alternate pins:
+//#define DIRA 8 // Direction control for motor A
+//#define PWMA 9 // PWM control (speed) for motor A
+//#define DIRB 7 // Direction control for motor B
+//#define PWMB 10 // PWM control (speed) for motor B
+
 void setup() {
+  setupArdumoto(); // Set all pins as outputs
+
   pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
   pinMode(echoPin, INPUT); // Sets the echoPin as an Input
   Serial.begin(9600);
@@ -52,4 +76,38 @@ int calculateDistance(){
   duration = pulseIn(echoPin, HIGH); // Reads the echoPin, returns the sound wave travel time in microseconds
   distance= duration*0.034/2;
   return distance;
+}
+
+// driveArdumoto drives 'motor' in 'dir' direction at 'spd' speed
+void driveArdumoto(byte motor, byte dir, byte spd) {
+  if (motor == MOTOR_A)
+  {
+    digitalWrite(DIRA, dir);
+    analogWrite(PWMA, spd);
+  }
+  else if (motor == MOTOR_B)
+  {
+    digitalWrite(DIRB, dir);
+    analogWrite(PWMB, spd);
+  }
+}
+
+// stopArdumoto makes a motor stop
+void stopArdumoto(byte motor) {
+  driveArdumoto(motor, 0, 0);
+}
+
+// setupArdumoto initialize all pins
+void setupArdumoto() {
+  // All pins should be setup as outputs:
+  pinMode(PWMA, OUTPUT);
+  pinMode(PWMB, OUTPUT);
+  pinMode(DIRA, OUTPUT);
+  pinMode(DIRB, OUTPUT);
+
+  // Initialize all pins as low:
+  digitalWrite(PWMA, LOW);
+  digitalWrite(PWMB, LOW);
+  digitalWrite(DIRA, LOW);
+  digitalWrite(DIRB, LOW);
 }
